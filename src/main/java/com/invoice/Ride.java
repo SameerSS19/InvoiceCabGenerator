6ef26ee53e90298615ef6f1b4f51b1d312df2c6c
SameerSS19 <sk3090019@gmail.com>
2021-06-16 11:54:29 +0530

    [Sameer]Add:Invoice Cab Generator.

---
+++ src/main/java/com/invoice/Ride.java
@@ -0,0 +1,11 @@
+package com.invoice;
+
+public class Ride {
+    public double distance;
+    public final int time;
+
+    public  Ride(double distance,int time){
+        this.distance = distance;
+        this.time = time;
+    }
+}
---
+++ src/test/java/com/invoice/InvoiceServiceTest.java
@@ -0,0 +1,37 @@
+package com.invoice;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+    InvoiceGenerator invoiceGenerator = null;
+    @Before
+    public void setup() throws Exception{
+        invoiceGenerator = new InvoiceGenerator();
+    }
+
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare(){
+        double distance = 2.0;
+        int time = 5;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(25,fare,0.0);
+    }
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare(){
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0.0);
+    }
+    @Test
+    public void givenMultipleRides_ShouldReturnInvoiceSummary(){
+        Ride[] rides= {new Ride(2.0, 5), new Ride(0.1, 1)
+        };
+       InvoiceSummary summary = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assert.assertEquals(expectedInvoiceSummary,summary);
+    }
+}
